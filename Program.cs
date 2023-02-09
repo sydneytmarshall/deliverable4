7893d090d262520b2f8f9dfef199ae9facb82c1c
sydneytmarshall <122946501+sydneytmarshall@users.noreply.github.com>
2023-02-09 16:15:04 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Deliverable_4;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+      
+
+        //Declaring arrays
+        int[] fibonacci = new int[25];
+        int[] even = new int[25];
+        int[] odd = new int[25];
+
+        
+        // Initialize fibonacci series
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 0; i < 25; i++)
+        {
+            even[i] = 2 * i;
+            odd[i] = 2 * i + 1;
+
+            if (i >= 2)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+        }
+        
+
+        Console.WriteLine("The first" + " " + 25.ToString() + " " +
+"Fibonacci numbers are: ");
+
+       
+
+        for (int i = 0; i < 25; i++)
+        {
+            Console.WriteLine("Fibonacci number (" + i + ") = " + fibonacci[i]);
+          
+        }
+
+    }
+}
+
